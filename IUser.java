950f4f8812eee1b2ca01bad70adac5b51011b5fc
dankvolta <maahdaoui@gmail.com>
2023-02-20 12:21:30 +0100

    Update IUser.java
    we added the method setCompany(ITaxiCompany) instead to passing it with the User constructor

--- IUser.java
+++ IUser.java
@@ -3,13 +3,19 @@ package taxify;
 public interface IUser {  
 	
 	public int getID();
+	
 	public String getFirstName();
 	public String getLastName();
-	public TaxiCompany getCompany();
+	
+	public ITaxiCompany getCompany();
+	public void setCompany(ITaxiCompany company);
+	
 	public boolean getService();
 	public void setService(boolean s);
+	
 	public void requestService();
-	public void rateService(IServices service);  // we use the interface instead of the class itself
+	public void rateService(IServices service);
+	
 	public String toString();
 	
 
